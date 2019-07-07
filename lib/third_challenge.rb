83cf42239ebf19bf8a784da96978e3d918b4938e
Stanley Luong <slluong@uw.edu>
2019-07-07 06:42:48 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -7,7 +7,7 @@ def third_challenge
       :hero_friends => [
          {name: "Benvolio", age: "17", attitude: "worried"},
          {name: "Mercutio", age: "18", attitude: "hot-headed"}
-      ])
+      ]
    },
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
